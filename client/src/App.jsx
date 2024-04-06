c82907cc4f5eeac72c94704b9604ae5a138b79b6
aditya9129 <adityagg9129@gmail.com>
2024-04-06 11:00:56 +0530

    initial files

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    
+      <div className='bg-[#0F172A] '>
+      
+      </div>
+     
+    
+  )
+}
+
+export default App
