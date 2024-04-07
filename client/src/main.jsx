d1de939ffd2257659749c0edc664288d4287b500
adityakhatana9@gmail.com <adityakhatana9@gmail.com>
2024-04-07 21:02:50 +0530

    removed strict mode from index.js file to stop rendering code editor 2 times

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-    </BrowserRouter>
-   
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
