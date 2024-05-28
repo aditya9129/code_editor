a15bc05d90ba1c5a8006809a93b2f236263fbc04
ayoaditya <adityakhatana9@gmail.com>
2024-05-28 22:11:43 +0530

    removed cursor error from codeEditor.js

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -10,9 +10,16 @@ const Editor = ({ socketRef, roomid, code }) => {
   const [wb, setWb] = useState(true);
 
   useEffect(() => {
+    if (editorRef.current) {
+      const editor = editorRef.current.editor;
+      const currentCode = editor.getValue();
 
-      editorRef.current.editor.setValue(code, 1); // Set code and move cursor to the end
-    
+      if (currentCode !== code) {
+        const cursorPosition = editor.getCursorPosition();
+        editor.setValue(code, -1); // Set code and maintain the current cursor position
+        editor.moveCursorToPosition(cursorPosition);
+      }
+    }
   }, [code]);
 
   const runCode = async () => {
@@ -93,102 +100,3 @@ const Editor = ({ socketRef, roomid, code }) => {
 };
 
 export default Editor;
-
-
-// import { useEffect, useRef, useState } from "react";
-// import AceEditor from "react-ace";
-// import "ace-builds/src-noconflict/theme-dracula";
-// import "ace-builds/src-noconflict/mode-javascript";
-// import io from "socket.io-client";
-// import { useParams } from "react-router-dom";
-// import debounce from "lodash.debounce";
-
-// const socket = io("http://localhost:5000");
-
-// const CodeEditor = () => {
-//   const { roomid } = useParams(); // Assume roomid is available from the URL
-//   const editorRef = useRef(null);
-//   const [output, setOutput] = useState("");
-//   const [initialCode, setInitialCode] = useState("");
-
-//   useEffect(() => {
-//     socket.emit("join", { roomid, username: "User" });
-
-//     socket.on("codeOutput", (data) => {
-//       setOutput(data.output);
-//     });
-
-//     socket.on("codeChange", (data) => {
-//       if (editorRef.current) {
-//         const currentCode = editorRef.current.editor.getValue();
-//         if (currentCode !== data.code) {
-//           editorRef.current.editor.setValue(data.code, 1); // 1 to move the cursor to the end of the code
-//         }
-//       }
-//     });
-
-//     socket.on("initialCode", (data) => {
-//       setInitialCode(data.code);
-//     });
-
-//     return () => {
-//       socket.off("codeOutput");
-//       socket.off("codeChange");
-//       socket.off("initialCode");
-//     };
-//   }, [roomid]);
-
-//   useEffect(() => {
-//     if (editorRef.current && initialCode) {
-//       editorRef.current.editor.setValue(initialCode, 1); // 1 to move the cursor to the end of the code
-//     }
-//   }, [initialCode]);
-
-//   const runCode = () => {
-//     const code = editorRef.current.editor.getValue();
-//     socket.emit("runCode", { code });
-//   };
-
-//   const debounceCodeChange = debounce((newCode) => {
-//     socket.emit("codeChange", { roomid, code: newCode });
-//   }, 500);
-
-//   const onCodeChange = (newCode) => {
-//     debounceCodeChange(newCode);
-//   };
-
-//   return (
-//     <div className="h-screen">
-//       <AceEditor
-//         ref={editorRef}
-//         mode="javascript"
-//         theme="dracula"
-//         name="editor"
-//         onChange={onCodeChange}
-//         fontSize={14}
-//         height="70vh"
-//         width="100%"
-//         showPrintMargin={false}
-//         showGutter={true}
-//         highlightActiveLine={true}
-//         value={initialCode}
-//         setOptions={{
-//           enableBasicAutocompletion: true,
-//           enableLiveAutocompletion: true,
-//           enableSnippets: false,
-//           showLineNumbers: true,
-//           tabSize: 2,
-//         }}
-//       />
-//       <button
-//         onClick={runCode}
-//         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-//       >
-//         Run Code
-//       </button>
-//       <pre className="mt-4 p-4 bg-gray-900 text-white rounded">{output}</pre>
-//     </div>
-//   );
-// };
-
-// export default CodeEditor;
