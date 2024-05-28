2d9ae1d26dfbde9a3a307830fe80e587272f1471
aditya9129 <adityagg9129@gmail.com>
2024-05-28 19:24:52 +0530

    sync

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -1,28 +1,160 @@
+import { useEffect, useRef, useState } from "react";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/theme-dracula";
+import "ace-builds/src-noconflict/mode-javascript";
+import WhiteBoard from "./WhiteBoard.jsx";
+
+const Editor = ({ socketRef, roomid, code }) => {
+  const editorRef = useRef(null);
+  const [output, setOutput] = useState("");
+  const [wb, setWb] = useState(true);
+
+  useEffect(() => {
+
+      editorRef.current.editor.setValue(code, 1); // Set code and move cursor to the end
+    
+  }, [code]);
+
+  const runCode = async () => {
+    const code = editorRef.current.editor.getValue();
+    const formattedCode = code.replace(/\n/g, " "); // Replace '\n' with actual line breaks
+
+    try {
+      const response = await fetch("http://localhost:5000/runCode", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code: formattedCode }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        setOutput(data.output);
+      } else {
+        setOutput(data.output);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setOutput(error.toString());
+    }
+  };
+
+  const syncCode = () => {
+    const code = editorRef.current.editor.getValue();
+    if (code) {
+      socketRef.current.emit("sync-change", {
+        roomid,
+        code,
+      });
+    }
+  };
+
+  return (
+    <div className="wb p-4">
+      <AceEditor
+        ref={editorRef}
+        className={wb ? "block" : "hidden"}
+        mode="javascript"
+        theme="dracula"
+        name="editor"
+        onChange={syncCode}
+        fontSize={14}
+        height="70vh"
+        width="100%"
+        showPrintMargin={false}
+        showGutter={true}
+        highlightActiveLine={true}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: false,
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+      />
+      {!wb && <WhiteBoard socketRef={socketRef} roomid={roomid} />}
+      <button
+        onClick={runCode}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Run Code
+      </button>
+      <button
+        onClick={() => setWb(!wb)}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        {wb ? "WhiteBoard" : "Editor"}
+      </button>
+      <div className="mt-4 p-4 bg-gray-900 text-white rounded overflow-auto max-h-28">
+        <pre>{output}</pre>
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
+
+
 // import { useEffect, useRef, useState } from "react";
 // import AceEditor from "react-ace";
 // import "ace-builds/src-noconflict/theme-dracula";
 // import "ace-builds/src-noconflict/mode-javascript";
 // import io from "socket.io-client";
+// import { useParams } from "react-router-dom";
+// import debounce from "lodash.debounce";
 
 // const socket = io("http://localhost:5000");
 
 // const CodeEditor = () => {
+//   const { roomid } = useParams(); // Assume roomid is available from the URL
 //   const editorRef = useRef(null);
 //   const [output, setOutput] = useState("");
+//   const [initialCode, setInitialCode] = useState("");
 
 //   useEffect(() => {
+//     socket.emit("join", { roomid, username: "User" });
+
 //     socket.on("codeOutput", (data) => {
 //       setOutput(data.output);
 //     });
 
+//     socket.on("codeChange", (data) => {
+//       if (editorRef.current) {
+//         const currentCode = editorRef.current.editor.getValue();
+//         if (currentCode !== data.code) {
+//           editorRef.current.editor.setValue(data.code, 1); // 1 to move the cursor to the end of the code
+//         }
+//       }
+//     });
+
+//     socket.on("initialCode", (data) => {
+//       setInitialCode(data.code);
+//     });
+
 //     return () => {
 //       socket.off("codeOutput");
+//       socket.off("codeChange");
+//       socket.off("initialCode");
 //     };
-//   }, []);
+//   }, [roomid]);
+
+//   useEffect(() => {
+//     if (editorRef.current && initialCode) {
+//       editorRef.current.editor.setValue(initialCode, 1); // 1 to move the cursor to the end of the code
+//     }
+//   }, [initialCode]);
 
 //   const runCode = () => {
 //     const code = editorRef.current.editor.getValue();
 //     socket.emit("runCode", { code });
+//   };
+
+//   const debounceCodeChange = debounce((newCode) => {
+//     socket.emit("codeChange", { roomid, code: newCode });
+//   }, 500);
+
+//   const onCodeChange = (newCode) => {
+//     debounceCodeChange(newCode);
 //   };
 
 //   return (
@@ -32,14 +164,14 @@
 //         mode="javascript"
 //         theme="dracula"
 //         name="editor"
-//         onChange={() => {}}
+//         onChange={onCodeChange}
 //         fontSize={14}
 //         height="70vh"
 //         width="100%"
 //         showPrintMargin={false}
 //         showGutter={true}
 //         highlightActiveLine={true}
-//         value=""
+//         value={initialCode}
 //         setOptions={{
 //           enableBasicAutocompletion: true,
 //           enableLiveAutocompletion: true,
@@ -60,101 +192,3 @@
 // };
 
 // export default CodeEditor;
-
-import { useEffect, useRef, useState } from "react";
-import AceEditor from "react-ace";
-import "ace-builds/src-noconflict/theme-dracula";
-import "ace-builds/src-noconflict/mode-javascript";
-import io from "socket.io-client";
-import { useParams } from "react-router-dom";
-import debounce from "lodash.debounce";
-
-const socket = io("http://localhost:5000");
-
-const CodeEditor = () => {
-  const { roomid } = useParams(); // Assume roomid is available from the URL
-  const editorRef = useRef(null);
-  const [output, setOutput] = useState("");
-  const [initialCode, setInitialCode] = useState("");
-
-  useEffect(() => {
-    socket.emit("join", { roomid, username: "User" });
-
-    socket.on("codeOutput", (data) => {
-      setOutput(data.output);
-    });
-
-    socket.on("codeChange", (data) => {
-      if (editorRef.current) {
-        const currentCode = editorRef.current.editor.getValue();
-        if (currentCode !== data.code) {
-          editorRef.current.editor.setValue(data.code, 1); // 1 to move the cursor to the end of the code
-        }
-      }
-    });
-
-    socket.on("initialCode", (data) => {
-      setInitialCode(data.code);
-    });
-
-    return () => {
-      socket.off("codeOutput");
-      socket.off("codeChange");
-      socket.off("initialCode");
-    };
-  }, [roomid]);
-
-  useEffect(() => {
-    if (editorRef.current && initialCode) {
-      editorRef.current.editor.setValue(initialCode, 1); // 1 to move the cursor to the end of the code
-    }
-  }, [initialCode]);
-
-  const runCode = () => {
-    const code = editorRef.current.editor.getValue();
-    socket.emit("runCode", { code });
-  };
-
-  const debounceCodeChange = debounce((newCode) => {
-    socket.emit("codeChange", { roomid, code: newCode });
-  }, 500);
-
-  const onCodeChange = (newCode) => {
-    debounceCodeChange(newCode);
-  };
-
-  return (
-    <div className="h-screen">
-      <AceEditor
-        ref={editorRef}
-        mode="javascript"
-        theme="dracula"
-        name="editor"
-        onChange={onCodeChange}
-        fontSize={14}
-        height="70vh"
-        width="100%"
-        showPrintMargin={false}
-        showGutter={true}
-        highlightActiveLine={true}
-        value={initialCode}
-        setOptions={{
-          enableBasicAutocompletion: true,
-          enableLiveAutocompletion: true,
-          enableSnippets: false,
-          showLineNumbers: true,
-          tabSize: 2,
-        }}
-      />
-      <button
-        onClick={runCode}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Run Code
-      </button>
-      <pre className="mt-4 p-4 bg-gray-900 text-white rounded">{output}</pre>
-    </div>
-  );
-};
-
-export default CodeEditor;
--- server.js
+++ server.js
@@ -1,3 +1,141 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const { exec } = require("child_process");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:5173", // Update the frontend port as per your setup
+    methods: ["GET", "POST"],
+  },
+});
+
+const userSocketMap = {};
+const roomChatHistory = {};
+
+function getAllConnectedClients(roomid) {
+  const room = io.sockets.adapter.rooms.get(roomid);
+  if (!room) return [];
+
+  return Array.from(room).map(socketid => ({
+    socketid,
+    username: userSocketMap[socketid],
+  }));
+}
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("join", ({ roomid, username }) => {
+    if (!username) {
+      socket.emit("redirect", "/");
+      return;
+    }
+    userSocketMap[socket.id] = username;
+    console.log(socket.id)
+    socket.join(roomid);
+    const clients = getAllConnectedClients(roomid);
+    if (roomChatHistory[roomid]) {
+      socket.emit("chat_history", roomChatHistory[roomid]);
+    }
+    clients.forEach(({ socketid }) => {
+      io.to(socketid).emit("joined", {
+        clients,
+        username,
+        socketid: socket.id,
+      });
+    });
+  });
+
+  socket.on("runCode", (data) => {
+    const { code } = data;
+
+    // Write the code to a temporary file
+    const fileName = "tempCode.js"; // Assuming JavaScript code for example
+    fs.writeFileSync(fileName, code);
+
+    // Execute the code using Node.js
+    exec(`node ${fileName}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        socket.emit("codeOutput", { output: `Error: ${stderr}` });
+        return;
+      }
+      socket.emit("codeOutput", { output: stdout });
+
+      // Clean up the temporary file
+      if (fs.existsSync(fileName)) {
+        fs.unlinkSync(fileName);
+      } else {
+        console.error(`File ${fileName} does not exist.`);
+      }
+    });
+  });
+
+  socket.on("message", ({ username, message, roomid, time, socketid }) => {
+    const chatMessage = { username, message, time, socketid };
+    console.log(chatMessage);
+    // Store the message in chat history
+    if (!roomChatHistory[roomid]) {
+      roomChatHistory[roomid] = [];
+    }
+    roomChatHistory[roomid].push(chatMessage);
+     io.to(roomid).emit("message", chatMessage);
+
+  });
+
+  socket.on('sync-change', ({ roomid, code }) => {
+    console.log(code,roomid);
+    io.to(roomid).emit('sync', code );
+  });
+  socket.on('board-sync', ({ data,roomid}) => {
+    
+    io.to(roomid).emit('b-sync', data);
+  });
+  
+  socket.on("disconnecting", () => {
+    const rooms = socket.rooms;
+    rooms.forEach((roomid) => {
+      io.to(roomid).emit("disconnected", {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
+    });
+  });
+
+  socket.on("disconnect", () => {
+    delete userSocketMap[socket.id];
+    console.log("Client disconnected");
+  });
+});
+
+app.post("/runCode", (req, res) => {
+  const { code } = req.body;
+
+  // For security reasons, never run untrusted code directly like this in a real application.
+  exec(`node -e "${code}"`, (error, stdout, stderr) => {
+    if (error) {
+      res.status(500).json({ output: stderr });
+    } else {
+      res.status(200).json({ output: stdout });
+    }
+  });
+});
+
+const port = 5000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
 // const express = require("express");
 // const http = require("http");
 // const socketIo = require("socket.io");
@@ -15,6 +153,7 @@
 
 // const userSocketMap = {};
 // const roomChatHistory = {};
+// const roomCodeMap = {}; // Store code for each room
 
 // function getAllConnectedClients(roomid) {
 //   return Array.from(io.sockets.adapter.rooms.get(roomid) || []).map(
@@ -30,17 +169,23 @@
 // io.on("connection", (socket) => {
 //   console.log(`New client connected: ${socket.id}`);
 
-//   socket.on("join", ({ roomid, username, id }) => {
+//   socket.on("join", ({ roomid, username }) => {
 //     if (!username) {
 //       socket.emit("redirect", "/");
 //       return;
 //     }
 //     userSocketMap[socket.id] = username;
 //     socket.join(roomid);
+
 //     const clients = getAllConnectedClients(roomid);
 //     if (roomChatHistory[roomid]) {
 //       io.to(socket.id).emit("chat_history", roomChatHistory[roomid]);
 //     }
+
+//     if (roomCodeMap[roomid]) {
+//       io.to(socket.id).emit("initialCode", { code: roomCodeMap[roomid] });
+//     }
+
 //     clients.forEach(({ socketid }) => {
 //       io.to(socketid).emit("joined", {
 //         clients,
@@ -52,12 +197,9 @@
 
 //   socket.on("runCode", (data) => {
 //     const { code } = data;
-
-//     // Write the code to a temporary file
 //     const fileName = "tempCode.js";
 //     fs.writeFileSync(fileName, code);
 
-//     // Execute the code using Node.js
 //     exec(`node ${fileName}`, (error, stdout, stderr) => {
 //       if (error) {
 //         console.error(`exec error: ${error}`);
@@ -66,7 +208,6 @@
 //       }
 //       socket.emit("codeOutput", { output: stdout });
 
-//       // Clean up the temporary file
 //       if (fs.existsSync(fileName)) {
 //         fs.unlinkSync(fileName);
 //       } else {
@@ -75,15 +216,22 @@
 //     });
 //   });
 
+//   socket.on("codeChange", ({ roomid, code }) => {
+//     roomCodeMap[roomid] = code;
+//     socket.to(roomid).emit("codeChange", { code });
+//   });
+
 //   socket.on("message", ({ username, message, roomid, time, socketid }) => {
 //     const chatMessage = { username, message, time, socketid };
 
-//     // Store the message in chat history
 //     if (!roomChatHistory[roomid]) {
 //       roomChatHistory[roomid] = [];
 //     }
 //     roomChatHistory[roomid].push(chatMessage);
 //     io.to(roomid).emit("message", { username, message, time, socketid });
+//   });
+//   socket.on("board-sync", ({ data, roomid }) => {
+//     io.to(roomid).emit("b-sync", data);
 //   });
 
 //   socket.on("disconnecting", () => {
@@ -104,124 +252,5 @@
 
 // const port = 5000;
 // server.listen(port, () => {
-//   console.log(`Server is running ${port}`);
+//   console.log(`Server is running on port ${port}`);
 // });
-
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const { exec } = require("child_process");
-const fs = require("fs");
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: "http://localhost:5173", // Update the frontend port as per your setup
-    methods: ["GET", "POST"],
-  },
-});
-
-const userSocketMap = {};
-const roomChatHistory = {};
-const roomCodeMap = {}; // Store code for each room
-
-function getAllConnectedClients(roomid) {
-  return Array.from(io.sockets.adapter.rooms.get(roomid) || []).map(
-    (socketid) => {
-      return {
-        socketid,
-        username: userSocketMap[socketid],
-      };
-    }
-  );
-}
-
-io.on("connection", (socket) => {
-  console.log(`New client connected: ${socket.id}`);
-
-  socket.on("join", ({ roomid, username }) => {
-    if (!username) {
-      socket.emit("redirect", "/");
-      return;
-    }
-    userSocketMap[socket.id] = username;
-    socket.join(roomid);
-
-    const clients = getAllConnectedClients(roomid);
-    if (roomChatHistory[roomid]) {
-      io.to(socket.id).emit("chat_history", roomChatHistory[roomid]);
-    }
-
-    if (roomCodeMap[roomid]) {
-      io.to(socket.id).emit("initialCode", { code: roomCodeMap[roomid] });
-    }
-
-    clients.forEach(({ socketid }) => {
-      io.to(socketid).emit("joined", {
-        clients,
-        username,
-        socketid: socket.id,
-      });
-    });
-  });
-
-  socket.on("runCode", (data) => {
-    const { code } = data;
-    const fileName = "tempCode.js";
-    fs.writeFileSync(fileName, code);
-
-    exec(`node ${fileName}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        socket.emit("codeOutput", { output: `Error: ${stderr}` });
-        return;
-      }
-      socket.emit("codeOutput", { output: stdout });
-
-      if (fs.existsSync(fileName)) {
-        fs.unlinkSync(fileName);
-      } else {
-        console.error(`File ${fileName} does not exist.`);
-      }
-    });
-  });
-
-  socket.on("codeChange", ({ roomid, code }) => {
-    roomCodeMap[roomid] = code;
-    socket.to(roomid).emit("codeChange", { code });
-  });
-
-  socket.on("message", ({ username, message, roomid, time, socketid }) => {
-    const chatMessage = { username, message, time, socketid };
-
-    if (!roomChatHistory[roomid]) {
-      roomChatHistory[roomid] = [];
-    }
-    roomChatHistory[roomid].push(chatMessage);
-    io.to(roomid).emit("message", { username, message, time, socketid });
-  });
-  socket.on("board-sync", ({ data, roomid }) => {
-    io.to(roomid).emit("b-sync", data);
-  });
-
-  socket.on("disconnecting", () => {
-    const rooms = [...socket.rooms];
-    rooms.forEach((roomid) => {
-      socket.in(roomid).emit("disconnected", {
-        socketId: socket.id,
-        username: userSocketMap[socket.id],
-      });
-    });
-  });
-
-  socket.on("disconnect", () => {
-    delete userSocketMap[socket.id];
-    console.log("Client disconnected");
-  });
-});
-
-const port = 5000;
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
