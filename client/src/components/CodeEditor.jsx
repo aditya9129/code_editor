a81dae6157e9f723990915e462b5bdc1824c8124
aditya9129 <adityagg9129@gmail.com>
2024-06-22 11:39:28 +0530

    css

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -1,12 +1,152 @@
 
-import  { useState, useEffect, useRef } from 'react';
+// import  { useState, useEffect, useRef } from 'react';
+// import AceEditor from 'react-ace';
+// import 'ace-builds/src-noconflict/mode-javascript';
+// import 'ace-builds/src-noconflict/theme-twilight'; // Updated theme
+// import WhiteBoard from './WhiteBoard'; // Make sure to import your WhiteBoard component
+// import debounce from 'lodash.debounce';
+
+// // eslint-disable-next-line react/prop-types
+// const Editor = ({ editorRef, socketRef, roomid, code }) => {
+//   const [output, setOutput] = useState("");
+//   const [wb, setWb] = useState(true);
+
+//   useEffect(() => {
+//     if (editorRef.current) {
+//       const editor = editorRef.current.editor;
+//       const currentCode = editor.getValue();
+      
+//       if (currentCode !== code) {
+//         const cursorPosition = editor.getCursorPosition();
+//         editor.setValue(code, -1); // Set code and maintain the current cursor position
+//         editor.moveCursorToPosition(cursorPosition);
+//       }
+//     }
+//   }, [code, editorRef]);
+
+//   const runCode = async () => {
+//     const code = editorRef.current.editor.getValue();
+//     const codeWithoutComments = code
+//       .split('\n')
+//       .map(line => line.replace(/\/\/.*$/, '').trim()) // Remove comments and trim each line
+//       .join(' ')
+//       .replace(/"/g, "'"); // Replace double quotes with single quotes
+  
+//     try {
+//       const response = await fetch("/runCode", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ code: codeWithoutComments }),
+//       });
+//       const data = await response.json();
+//       if (response.ok) {
+//         setOutput(data.output);
+//       } else {
+//         setOutput(data.output);
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//       setOutput(error.toString());
+//     }
+//   };
+  
+
+//   const syncCode = () => {
+//     const code = editorRef.current.editor.getValue();
+    
+//     if (code ) { // Check if the current code is different from the previous code
+      
+//       socketRef.current.emit("sync-change", {
+//         roomid,
+//         code,
+//       });
+//     }
+//   };
+
+//   // Create a debounced version of syncCode
+//   const debouncedSyncCode = useRef(debounce(syncCode, 200)).current;
+
+//   return (
+//     <div className="bg-[#141414] border rounded-lg m-1 space-y-4">
+//       <div className=''>
+//         <AceEditor
+//           ref={editorRef}
+//           className={wb ? "block" : "hidden"}
+//           mode="javascript"
+//           theme="twilight" // Updated theme
+//           name="editor"
+//           onChange={syncCode}
+//           fontSize={14}
+//           height="71vh"
+//           width="100%"
+//           showPrintMargin={false}
+//           showGutter={true}
+//           highlightActiveLine={true}
+//           setOptions={{
+//             enableBasicAutocompletion: true,
+//             enableLiveAutocompletion: true,
+//             enableSnippets: false,
+//             showLineNumbers: true,
+//             tabSize: 2,
+//           }}
+//         />
+//       </div>
+//       {!wb && <WhiteBoard socketRef={socketRef} roomid={roomid} className=" w-full" />}
+//       <div className="flex flex-row-reverse mx-2 bg-[#141414] ">
+//         {wb && (
+//           <button
+            
+//             className="mt-4 mx-2 px-4 mb-2 bg-black hover:bg-[#363636] text-white rounded transition duration-300"
+//           >
+//             Run Code
+//           </button>
+//         )}
+//         <button
+//           onClick={() => setWb(!wb)}
+//           className="mt-4 px-4 py-2 mb-2 bg-black hover:bg-[#363636] text-white rounded transition duration-300"
+//         >
+//           {wb ? "WhiteBoard" : "Editor"}
+//         </button>
+//       </div>
+//       {wb && (
+//         <div className="bg-[#1f1f1f] text-white rounded overflow-auto h-28">
+//           <p className="text-[#3B3B3B]">{'//output'}</p>
+//           <pre>{output}</pre>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Editor;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { useState, useEffect, useRef } from 'react';
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-javascript';
-import 'ace-builds/src-noconflict/theme-twilight'; // Updated theme
-import WhiteBoard from './WhiteBoard'; // Make sure to import your WhiteBoard component
+import 'ace-builds/src-noconflict/theme-twilight';
+import WhiteBoard from './WhiteBoard';
 import debounce from 'lodash.debounce';
 
-// eslint-disable-next-line react/prop-types
 const Editor = ({ editorRef, socketRef, roomid, code }) => {
   const [output, setOutput] = useState("");
   const [wb, setWb] = useState(true);
@@ -15,7 +155,7 @@ const Editor = ({ editorRef, socketRef, roomid, code }) => {
     if (editorRef.current) {
       const editor = editorRef.current.editor;
       const currentCode = editor.getValue();
-      
+
       if (currentCode !== code) {
         const cursorPosition = editor.getCursorPosition();
         editor.setValue(code, -1); // Set code and maintain the current cursor position
@@ -24,40 +164,38 @@ const Editor = ({ editorRef, socketRef, roomid, code }) => {
     }
   }, [code, editorRef]);
 
-  // const runCode = async () => {
-  //   const code = editorRef.current.editor.getValue();
-  //   const codeWithoutComments = code
-  //     .split('\n')
-  //     .map(line => line.replace(/\/\/.*$/, '').trim()) // Remove comments and trim each line
-  //     .join(' ')
-  //     .replace(/"/g, "'"); // Replace double quotes with single quotes
-  
-  //   try {
-  //     const response = await fetch("https://code-editor-1-koew.onrender.com/runCode", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ code: codeWithoutComments }),
-  //     });
-  //     const data = await response.json();
-  //     if (response.ok) {
-  //       setOutput(data.output);
-  //     } else {
-  //       setOutput(data.output);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //     setOutput(error.toString());
-  //   }
-  // };
-  
+  const runCode = async () => {
+    const code = editorRef.current.editor.getValue();
+    const codeWithoutComments = code
+      .split('\n')
+      .map(line => line.replace(/\/\/.*$/, '').trim()) // Remove comments and trim each line
+      .join(' ')
+      .replace(/"/g, "'"); // Replace double quotes with single quotes
+
+    try {
+      const response = await fetch("/runCode", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code: codeWithoutComments }),
+      });
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setOutput(data.output);
+    } catch (error) {
+      console.error("Error:", error);
+      setOutput(error.toString());
+    }
+  };
 
   const syncCode = () => {
     const code = editorRef.current.editor.getValue();
-    
-    if (code ) { // Check if the current code is different from the previous code
-      
+    if (code) {
       socketRef.current.emit("sync-change", {
         roomid,
         code,
@@ -65,7 +203,6 @@ const Editor = ({ editorRef, socketRef, roomid, code }) => {
     }
   };
 
-  // Create a debounced version of syncCode
   const debouncedSyncCode = useRef(debounce(syncCode, 200)).current;
 
   return (
@@ -75,9 +212,9 @@ const Editor = ({ editorRef, socketRef, roomid, code }) => {
           ref={editorRef}
           className={wb ? "block" : "hidden"}
           mode="javascript"
-          theme="twilight" // Updated theme
+          theme="twilight"
           name="editor"
-          onChange={syncCode}
+          onChange={debouncedSyncCode}
           fontSize={14}
           height="71vh"
           width="100%"
@@ -97,7 +234,7 @@ const Editor = ({ editorRef, socketRef, roomid, code }) => {
       <div className="flex flex-row-reverse mx-2 bg-[#141414] ">
         {wb && (
           <button
-            
+            onClick={runCode}
             className="mt-4 mx-2 px-4 mb-2 bg-black hover:bg-[#363636] text-white rounded transition duration-300"
           >
             Run Code
@@ -121,3 +258,4 @@ const Editor = ({ editorRef, socketRef, roomid, code }) => {
 };
 
 export default Editor;
+
